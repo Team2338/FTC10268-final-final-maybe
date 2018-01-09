99f4b2272a440af24a62c5823de63713160f3752
SamTheCatssassin <31957321+SamTheCatssassin@users.noreply.github.com>
2018-01-09 15:09:56 -0600

    holdon equipped

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTemp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTemp.java
@@ -93,6 +93,8 @@ public class TeleOpTemp extends OpMode {
     double tafo = 0;
     double tafn = 0;
 
+    double holdon = 0;
+
     /* Declare OpMode members. */
     private ElapsedTime runtime = new ElapsedTime();
 
